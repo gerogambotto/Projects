5d215c358d834cd8ec4b028605ccdaba6138709c
Geronimo Gambotto <gero.gambotto@gmail.com>
2023-04-12 15:51:23 -0300

    Twitter Follow Card

---
+++ TwitterFollowCard/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [  
+    {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: false
+    },
+    {
+    userName: 'GeroGambotto',
+    name: 'Geronimo Gambotto',
+    isFollowing: false
+    },
+    {
+    userName: 'sebadorsch',
+    name: 'Sebastian Dorsch',
+    isFollowing: false
+    },
+    {
+    userName: 'maxsantoro',
+    name: 'Maximiliano Santoro',
+    isFollowing: false
+    }
+]
+
+export function App () {
+
+    
+
+    return (
+        <>
+        <section className='App'>
+            {
+            users.map(({userName, name, isFollowing}) => (
+                
+                
+                    <TwitterFollowCard
+                    key={userName}
+                    userName={userName}
+                    initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                
+            ))
+            }
+        </section>
+        </>
+    )
+}
